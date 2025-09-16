959b11ff3eb7b69bc3aaf85307fe0df7ebf981dc
eshaan s subhedar <subhedareshaan@gmail.com>
2025-09-16 12:08:38 +0530

    Add files via upload

---
+++ pathfinder/frontend/src/pages/admin/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography, Box } from '@mui/material'
+
+export default function AdminDashboard() {
+  return (
+    <Box>
+      <Typography variant="h4">Admin Dashboard</Typography>
+      <Typography>This page is under development.</Typography>
+    </Box>
+  )
+}
+
+
---
+++ pathfinder/frontend/src/pages/admin/Users.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography, Box } from '@mui/material'
+
+export default function AdminUsers() {
+  return (
+    <Box>
+      <Typography variant="h4">Admin Users</Typography>
+      <Typography>This page is under development.</Typography>
+    </Box>
+  )
+}
+
+
