a7f22318bb37a121d1dbd228f65d709e8445ed7f
eshaan s subhedar <subhedareshaan@gmail.com>
2025-09-16 12:10:22 +0530

    Create Applications.tsx

---
+++ pathfinder/frontend/src/pages/company/Applications.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography, Box } from '@mui/material'
+
+export default function CompanyApplications() {
+  return (
+    <Box>
+      <Typography variant="h4">Company Applications</Typography>
+      <Typography>This page is under development.</Typography>
+    </Box>
+  )
+}
+
+
