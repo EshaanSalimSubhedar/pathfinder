2d5c14a0a925a451bf45994e37b8f986ef2efb20
eshaan s subhedar <subhedareshaan@gmail.com>
2025-09-16 12:17:32 +0530

    Create Chat.tsx

---
+++ pathfinder/frontend/src/pages/shared/Chat.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography, Box } from '@mui/material'
+
+export default function ChatPage() {
+  return (
+    <Box>
+      <Typography variant="h4">Chat</Typography>
+      <Typography>This page is under development.</Typography>
+    </Box>
+  )
+}
+
+
