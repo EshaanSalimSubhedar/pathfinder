c774410a17537b0eade029a1ae0bb47cc9383af3
eshaan s subhedar <subhedareshaan@gmail.com>
2025-09-16 12:17:47 +0530

    Add files via upload

---
+++ pathfinder/frontend/src/pages/shared/InternshipDetails.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography, Box } from '@mui/material'
+
+export default function InternshipDetails() {
+  return (
+    <Box>
+      <Typography variant="h4">Internship Details</Typography>
+      <Typography>This page is under development.</Typography>
+    </Box>
+  )
+}
+
+
